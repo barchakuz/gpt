3d7abb111d03a81818c2460419ded6557d5d33c6
Barchakuz <behroze.barcha@gmail.com>
2024-05-30 22:40:42 +0500

    behroze

--- my-app/src/containers/footer/Footer.jsx
+++ my-app/src/containers/footer/Footer.jsx
@@ -1,8 +1,49 @@
 import React from 'react'
 import './footer.css'
+import gpt3Logo from '../../assets/Logo2.png';
 function Footer() {
   return (
-    <div>Footer</div>
+   <>
+    <div className="gpt3__footer section__padding">
+    <div className="gpt3__footer-heading">
+      <h1 className="gradient__text">Do you want to step in to the future before others</h1>
+    </div>
+
+    <div className="gpt3__footer-btn">
+      <p>Request Early Access</p>
+    </div>
+
+    <div className="gpt3__footer-links">
+      <div className="gpt3__footer-links_logo">
+        <img src={gpt3Logo} alt="gpt3_logo" />
+        <p>Behroze Aslam, <br /> All Rights Reserved</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Links</h4>
+        <p>Overons</p>
+        <p>Social Media</p>
+        <p>Counters</p>
+        <p>Contact</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Company</h4>
+        <p>Terms & Conditions </p>
+        <p>Privacy Policy</p>
+        <p>Contact</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Get in touch</h4>
+        <p>Dha Phase Islamabad</p>
+        <p>031-75037080</p>
+        <p>behroze.barcha@gmail.com</p>
+      </div>
+    </div>
+
+    <div className="gpt3__footer-copyright">
+      <p>@2024 GPT-3. All rights reserved.</p>
+    </div>
+  </div>
+   </>
   )
 }
 
--- my-app/src/containers/possibilities/Possibilities.jsx
+++ my-app/src/containers/possibilities/Possibilities.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 import './possibilites.css'
+import possibilityImage from '../../assets/Fi.png';
 function Possibilities() {
   return (
-    <div>Possibilities</div>
+  <>
+      <div className="gpt3__possibility section__padding" id="possibility">
+    <div className="gpt3__possibility-image">
+      <img src={possibilityImage} alt="possibility" />
+    </div>
+    <div className="gpt3__possibility-content">
+      <h4>Request Early Access to Get Started</h4>
+      <h1 className="gradient__text">The possibilities are <br /> beyond your imagination</h1>
+      <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+      <h4>Request Early Access to Get Started</h4>
+    </div>
+  </div>
+  </>
   )
 }
 
